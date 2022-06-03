57b5d1800347c3d3ab58a3b1d2ab8705751353ad
wakki <ytk0202@docomo.ne.jp>
2022-06-04 00:46:34 +0900

    mac first-commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
               </div>
               <div className='lg:w-3/12 lg:m-auto'>
                 <h1 className='text-lg lg:text-2xl font-bold mb-5 mt-8 lg:text-center'>
-                <span className='border-b-2 border-black'>ブランディング</span>
+                <span className='border-b-2 border-black'>プランニング</span>
               </h1>
               <p className='line-relaxed lg:text-lg text-sm'>
                 クリエイティブの力で、社会のあらゆる変化と挑戦を推進していく。 GOは、広告領域に留まらず事業全体をクリエイティブの力で成長・進化していきます。 売上や企業価値だけでなく、事業を通して社会・顧客・生活者の幸福を追求します。
